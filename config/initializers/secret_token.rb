d8ab7e7ff81d32b167929cd339b23bb760b02ead
mostlyclicks <carlos@mostlyclicks.com>
2013-11-19 17:01:17 -0600

    initial refinerycms commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trhobbies::Application.config.secret_token = '5a07063e2125b1a63c19b3bb806dfe543f7543be5c94cc69a9ed1be5c7b2a50240e324b38122c5f418aa787ffb8808887ac6603d64fc866b328d26df45ea6464'
